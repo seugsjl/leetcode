3cede2b72e02a5256ec46488d1c4e9bfff6c81d9
seugsjl <646988177@qq.com>
2015-04-27 17:15:57 +0800

    dir change

---
+++ 0-99/LC015.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int> > threeSum(vector<int> &num) 
+    {
+        sort(num.begin(),num.end());
+        
+    }
+};
+
+int main()
+{
+}
